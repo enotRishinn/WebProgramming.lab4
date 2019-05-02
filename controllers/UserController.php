f4e8d28d716d35ca2624e6ec8920debceef48c3b
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-05-02 21:28:53 +0300

    working user controller

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -4,7 +4,7 @@ namespace app\controllers;
 
 use yii\web\Controller;
 
-class PostController extends Controller {
+class UserController extends Controller {
 
   public function actionIndex() {
     return $this->render('index');
