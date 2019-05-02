c73678ca66ca8193d699b8a93f0836782383f879
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-05-02 20:43:46 +0300

    new commit

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=weblab4',
-    'username' => 'WebLab4',
-    'password' => '12345678987654321',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
