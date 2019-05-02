d29015de9a12ec9713e7df7bcbae337f7508a112
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-05-03 00:13:06 +0300

    create add, news, delete and admin url

--- views/user/admin.php
+++ views/user/admin.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 <div class="wrap">
   <div class="container">
     <ul class="nav nav-pills">
-      <li role="presentation" class="active" ><?= Html::a('Новости', ['user/index']) ?></li>
-      <li role="presentation" class="active" ><?= Html::a('Добавление новостей', ['user/index']) ?></li>
-      <li role="presentation" class="active" ><?= Html::a('Удаление новостей', ['user/index']) ?></li>
+      <li role="presentation" class="active" ><?= Html::a('Новости', ['user/news']) ?></li>
+      <li role="presentation" class="active" ><?= Html::a('Добавление новостей', ['user/add']) ?></li>
+      <li role="presentation" class="active" ><?= Html::a('Удаление новостей', ['user/delete']) ?></li>
     </ul>
   </div>
 </div>
