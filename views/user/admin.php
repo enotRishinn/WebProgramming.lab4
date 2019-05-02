ae0b77a2f7c91a563e5464cb9657b5fa7d4cba35
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-05-02 23:45:57 +0300

    change nav color

--- views/user/admin.php
+++ views/user/admin.php
@@ -4,10 +4,10 @@ use yii\helpers\Html;
 
 <div class="wrap">
   <div class="container">
-    <ul class="nav nav-pills black">
-      <li role="presentation" class="active"><?= Html::a('Новости', ['user/index']) ?></li>
-      <li role="presentation" class="active"><?= Html::a('Добавление новости', ['user/index']) ?></li>
-      <li role="presentation" class="active"><?= Html::a('Удаление новости', ['user/index']) ?></li>
+    <ul class="nav nav-pills">
+      <li role="presentation" class="active" ><?= Html::a('Новости', ['user/index']) ?></li>
+      <li role="presentation" class="active" ><?= Html::a('Добавление новостей', ['user/index']) ?></li>
+      <li role="presentation" class="active" ><?= Html::a('Удаление новостей', ['user/index']) ?></li>
     </ul>
   </div>
 </div>
