34a3707ad90bb6896878a709ab7cf0c75e58877d
Emtseva Victoria <enot.rishinn@yandex.ru>
2019-05-03 21:10:59 +0300

    new nav

--- views/user/add.php
+++ views/user/add.php
@@ -3,15 +3,13 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 ?>
 
-<div class="wrap">
-  <div class="container">
+<div class="wrap" ><br><br>
     <ul class="nav nav-pills">
       <li role="presentation" class="active" ><?= Html::a('Новости', ['user/admin']) ?></li>
       <li role="presentation" class="active" id="choose" ><?= Html::a('Добавление новостей', ['user/add']) ?></li>
       <li role="presentation" class="active" ><?= Html::a('Редактирование и удаление новостей', ['post/index']) ?></li>
     </ul>
-  </div>
-</div>
+</div><br><br>
 
 <?php if (Yii::$app->session->hasFlash('success')): ?>
   <div class="alert alert-success alert-dismissible fade show" role="alert">
--- views/user/admin.php
+++ views/user/admin.php
@@ -2,15 +2,13 @@
 use yii\helpers\Html;
 ?>
 
-<div class="wrap">
-  <div class="container">
+<div class="wrap" ><br><br>
     <ul class="nav nav-pills">
-      <li role="presentation" class="active" id="choose"><?= Html::a('Новости', ['user/admin']) ?></li>
-      <li role="presentation" class="active" ><?= Html::a('Добавление новостей', ['user/add']) ?></li>
+      <li role="presentation" class="active" ><?= Html::a('Новости', ['user/admin']) ?></li>
+      <li role="presentation" class="active" id="choose" ><?= Html::a('Добавление новостей', ['user/add']) ?></li>
       <li role="presentation" class="active" ><?= Html::a('Редактирование и удаление новостей', ['post/index']) ?></li>
     </ul>
-  </div>
-</div>
+</div><br><br>
 
 <?php foreach ($cats as $cat) {
   echo '<h1>'.$cat->title.'<br></h1>';
